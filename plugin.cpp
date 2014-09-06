1f681723e9f36ac27a628efeefa9b1f666adbf15
lin <linzj@ucweb.com>
2014-09-06 11:08:10 +0800

    * plugin.cpp: printing the test function all right

--- plugin.cpp
+++ plugin.cpp
@@ -1,34 +1,41 @@
-#include "tree-pass.h"
 #include "gcc-plugin.h"
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tm.h"
+#include "tree.h"
+#include "tree-pass.h"
+#include "tree-cfg.h"
 #include "function.h"
 #include "gimple-expr.h"
 
 static struct pass_data mypass = {
-  GIMPLE_PASS,     /* type */
-  "mypass",        /* name */
-  OPTGROUP_NONE,   /* optinfo_flags */
-  false,           /* has_gate */
-  true,            /* has_execute */
-  TV_NONE,         /* tv_id */
-  PROP_gimple_any, /* properties_required */
-  0,               /* properties_provided */
-  0,               /* properties_destroyed */
-  0,               /* todo_flags_start */
-  0,               /* todo_flags_finish */
+  GIMPLE_PASS,              /* type */
+  "gen_trace",                 /* name */
+  OPTGROUP_NONE,            /* optinfo_flags */
+  false,                    /* has_gate */
+  true,                     /* has_execute */
+  TV_NONE,                  /* tv_id */
+  PROP_gimple_any,          /* properties_required */
+  0,                        /* properties_provided */
+  0,                        /* properties_destroyed */
+  TODO_mark_first_instance, /* todo_flags_start */
+  0,                        /* todo_flags_finish */
 };
 extern gcc::context *g;
+int plugin_is_GPL_compatible;
 
 static unsigned int
 execute_trace ()
 {
-  dump_function_to_file (current_function_decl, stderr, -1);
+  dump_function_to_file (current_function_decl, stderr, TDF_TREE | TDF_BLOCKS | TDF_VERBOSE);
   exit (0);
 }
 
 class trace_pass : public gimple_opt_pass
 {
 public:
-  trace_pass (pass_data *mydata, gcc::context *context)
+  trace_pass (pass_data &mydata, gcc::context *context)
       : gimple_opt_pass (mydata, context)
   {
   }
@@ -44,12 +51,12 @@ plugin_init (struct plugin_name_args *plugin_info,
              struct plugin_gcc_version *version)
 {
   struct register_pass_info pass_info
-      = { new gimple_opt_pass (&mypass, g), "omplower", 6,
-          PASS_POS_INSERT_BEFORE };
+      = { new trace_pass (mypass, g), "omplower", 1, PASS_POS_INSERT_BEFORE };
 
   /* Code to fill in the pass_info object with new pass information.  */
 
   /* Register the new pass.  */
   register_callback (plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL,
                      &pass_info);
+  return 0;
 }
