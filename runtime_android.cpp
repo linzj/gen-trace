5795d4951125e71ac9b404ea22a54475adbf5795
linzj <linzj@ucweb.com>
2014-09-10 13:41:13 +0800

    * runtime_android.cpp: New File. Runtime implement for android.

---
+++ runtime_android.cpp
@@ -0,0 +1,23 @@
+#include <new>
+#define __STDC_FORMAT_MACROS
+#define CTRACE_FILE_NAME "/sdcard/trace.json"
+#define CTRACE_OMIT_JITTER 100000
+#define CTRACE_THREAD_SUPPORTED
+#include "ctrace.h"
+
+extern "C" {
+extern void __start_ctrace__ (void *c, const char *name);
+extern void __end_ctrace__ (CTrace *c, const char *name);
+}
+
+void
+__start_ctrace__ (void *c, const char *name)
+{
+  new (c) CTrace ("profile", name);
+}
+
+void
+__end_ctrace__ (CTrace *c, const char *name)
+{
+  c->~CTrace ();
+}
