98409b8f5402f4623c7f9f01c2ed16e656a25bef
linzj <linzj@ucweb.com>
2014-12-29 13:58:05 +0800

    * act_on_elf.sh: Make elfs go to back of the options

--- act_on_elf.sh
+++ act_on_elf.sh
@@ -2,12 +2,13 @@
 
 if [ $# -ne 3 ]
     then
-        echo '<elf file> <code modification log file path (could be empty string)> <pause seconds before start>' 2>&1
+        echo '<code modification log file path (could be empty string)> <pause seconds before start> <elf file>' 2>&1
         exit 1
 fi
 
-echo $2 > trace.config
-echo $3 >> trace.config
+echo $1 > trace.config
+echo $2 >> trace.config
+shift 2
 echo ${1##*/}  >> trace.config
 MY_PATH=$(dirname $0)
 readelf -sW $1 | python $MY_PATH/filter_readelf.py| c++filt  | python ${MY_PATH}/post_filter.py >> trace.config
