2534fccaf159578200ddcc177b725e6c21c0a618
linzj <linzj@ucweb.com>
2015-01-06 14:19:53 +0800

    * post_filter.py: Add black/white name list.

--- post_filter.py
+++ post_filter.py
@@ -1,23 +1,22 @@
 import sys
-key_words = (
-'operator',
-'alloc',
-'free',
-'mallinfo',
-'mallopt',
-'memalign',
-'Allocate',
-'Lock',
-'CheckMallocResult',
-'tc_',
-'GetSizeWithCallback',
-'PackedCache',
-'SIGCHLDHandler',
-'base',
-'std',
+white_key_words = (
+'Prerender',
 )
-def has_key_word(_str):
-    for key_word in key_words:
+
+black_key_words = (
+'std::',
+)
+
+def has_white_key_word(_str):
+    if not white_key_words:
+        return True
+    for key_word in white_key_words:
+        if key_word in _str:
+            return True
+    return False
+
+def has_black_key_words(_str):
+    for key_word in black_key_words:
         if key_word in _str:
             return True
     return False
@@ -34,7 +33,9 @@ def filter_file (input_file, output_file):
         l3 = input_file.readline ()
         if not l1 or not l2 or not l3:
             break
-        if l3.startswith ('__') or has_key_word (l3):
+        if l3.startswith ('__') or has_black_key_words (l3):
+            continue
+        if not has_white_key_word(l3):
             continue
         print >>output_file, l1.rstrip ()
         print >>output_file, l2.rstrip ()
